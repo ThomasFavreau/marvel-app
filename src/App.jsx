e65b555993e05e5d7f200aa3f18bf7d6fccea70b
Thomas Favreau <thomas.favreau@etu.univ-poitiers.fr>
2024-11-18 16:55:04 +0100

    test-reglage-erreur

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import routes from './routes'
+import routes from './Routes'
 
 // Create a router that uses the client side history strategy for
 const router = createBrowserRouter(routes)
