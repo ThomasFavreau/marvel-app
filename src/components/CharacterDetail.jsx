63d6c3717c73703035ba8f34b23c85d510572022
Thomas Favreau <thomas.favreau@etu.univ-poitiers.fr>
2024-12-02 17:41:59 +0100

    Ajout_date

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,10 +1,9 @@
+import { formatDate } from '../fonctions/fonction_date';
+
 function CharacterDetail({ character = {} }) {
     if (!character || Object.keys(character).length === 0) {
         return <div>No character</div>;
     } else {
-        const date = new Date(character.modified);
-        const options = { year: 'numeric', month: 'short', day: 'numeric' };
-        const formattedDate = date.toLocaleDateString('en-US', options);
         return (
             <div>
                 <h2>{character.name}</h2>
@@ -12,7 +11,7 @@ function CharacterDetail({ character = {} }) {
                     character.thumbnail && <img src={`${character.thumbnail.path}/standard_large.${character.thumbnail.extension}`} alt={character.name} />
                 }
                 <p>{character.description}</p>
-                <p>{formattedDate}</p>
+                <p>{formatDate(character.modified)}</p>
             </div>
         );
     }
--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -1,17 +1,15 @@
 import { Link } from "react-router-dom";
+import { formatDate } from '../fonctions/fonction_date';
 
 export function CharactersList({ characters = [] }) {
   return (
     <ul id="characters">
       {characters.map((character) => {
-        const date = new Date(character.modified);
-        const options = { year: 'numeric', month: 'short', day: 'numeric' };
-        const formattedDate = date.toLocaleDateString('en-US', options);
-        
+         
         return (
           <li key={character.id}>
             <Link to={`/characters/${character.id}`}>
-            <strong>{character.name}</strong> - <small>{formattedDate}</small>
+            <strong>{character.name}</strong> - <small>{formatDate(character.modified)}</small>
             </Link>
           </li>
         );
